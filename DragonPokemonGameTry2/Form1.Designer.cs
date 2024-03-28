6525832055d36e6c1048b6ef8899de0628c7aa3b
RaeezLachporia <88308343+RaeezLachporia@users.noreply.github.com>
2024-03-28 23:23:09 +0200

    AddedSome stuff

--- DragonPokemonGameTry2/Form1.Designer.cs
+++ DragonPokemonGameTry2/Form1.Designer.cs
@@ -28,31 +28,70 @@
         /// </summary>
         private void InitializeComponent()
         {
-            button1 = new Button();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            BTNPlay = new Button();
+            this.pictureBox1 = new PictureBox();
+            BTNBestiary = new Button();
+            BTNQuit = new Button();
+            ((System.ComponentModel.ISupportInitialize)this.pictureBox1).BeginInit();
             SuspendLayout();
             // 
-            // button1
+            // BTNPlay
             // 
-            button1.Location = new Point(363, 196);
-            button1.Name = "button1";
-            button1.Size = new Size(75, 23);
-            button1.TabIndex = 0;
-            button1.Text = "button1";
-            button1.UseVisualStyleBackColor = true;
+            BTNPlay.Location = new Point(332, 176);
+            BTNPlay.Name = "BTNPlay";
+            BTNPlay.Size = new Size(114, 61);
+            BTNPlay.TabIndex = 0;
+            BTNPlay.Text = "Play";
+            BTNPlay.UseVisualStyleBackColor = true;
+            BTNPlay.Click += button1_Click;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            this.pictureBox1.Location = new Point(158, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new Size(442, 136);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // BTNBestiary
+            // 
+            BTNBestiary.Location = new Point(332, 272);
+            BTNBestiary.Name = "BTNBestiary";
+            BTNBestiary.Size = new Size(114, 61);
+            BTNBestiary.TabIndex = 2;
+            BTNBestiary.Text = "Bestiary";
+            BTNBestiary.UseVisualStyleBackColor = true;
+            // 
+            // BTNQuit
+            // 
+            BTNQuit.Location = new Point(332, 362);
+            BTNQuit.Name = "BTNQuit";
+            BTNQuit.Size = new Size(114, 61);
+            BTNQuit.TabIndex = 3;
+            BTNQuit.Text = "Quit";
+            BTNQuit.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
-            Controls.Add(button1);
+            Controls.Add(BTNQuit);
+            Controls.Add(BTNBestiary);
+            Controls.Add(this.pictureBox1);
+            Controls.Add(BTNPlay);
             Name = "Form1";
             Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)this.pictureBox1).EndInit();
             ResumeLayout(false);
         }
 
         #endregion
 
-        private Button button1;
+        private Button BTNPlay;
+        private Button BTNBestiary;
+        private Button BTNQuit;
     }
 }
--- DragonPokemonGameTry2/Form1.cs
+++ DragonPokemonGameTry2/Form1.cs
@@ -6,5 +6,10 @@ namespace DragonPokemonGameTry2
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
