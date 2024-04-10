1d3f10d023a833bf373ff9427ef0c82734b6b670
RaeezLachporia <88308343+RaeezLachporia@users.noreply.github.com>
2024-04-10 17:08:22 +0200

    added new naming conventions

--- DragonPokemonGameTry2/PlayerInfoScreen.Designer.cs
+++ DragonPokemonGameTry2/PlayerInfoScreen.Designer.cs
@@ -28,109 +28,125 @@
         /// </summary>
         private void InitializeComponent()
         {
-            groupBox1 = new GroupBox();
-            groupBox2 = new GroupBox();
-            groupBox3 = new GroupBox();
-            groupBox4 = new GroupBox();
-            BTNP1Details = new Button();
-            BTNP2Details = new Button();
-            checkboxP1fire = new CheckBox();
-            checkBoxP1Wind = new CheckBox();
-            checkBoxP1Earth = new CheckBox();
-            checkBoxP1Water = new CheckBox();
-            label1 = new Label();
-            label2 = new Label();
-            TXTPlayer1Name = new TextBox();
+            GBP1details = new GroupBox();
             TXTPlayer1DragonName = new TextBox();
+            TXTPlayer1Name = new TextBox();
+            label2 = new Label();
+            label1 = new Label();
+            GBP1dragontype = new GroupBox();
+            checkBoxP1Water = new CheckBox();
+            checkBoxP1Earth = new CheckBox();
+            checkBoxP1Wind = new CheckBox();
+            checkboxP1fire = new CheckBox();
+            BTNP1Details = new Button();
+            GBP2details = new GroupBox();
             TXTPlayer2DragonName = new TextBox();
-            TXTPlayer2Name = new TextBox();
-            label3 = new Label();
-            label4 = new Label();
+            GBP2dragontype = new GroupBox();
             checkBoxP2Water = new CheckBox();
+            BTNP2Details = new Button();
             checkBoxP2Earth = new CheckBox();
             checkBoxP2Wind = new CheckBox();
             checkBoxP2Fire = new CheckBox();
+            TXTPlayer2Name = new TextBox();
+            label4 = new Label();
+            label3 = new Label();
             BTNStartGame = new Button();
             BTNBack = new Button();
-            groupBox1.SuspendLayout();
-            groupBox2.SuspendLayout();
-            groupBox3.SuspendLayout();
-            groupBox4.SuspendLayout();
+            GBP1details.SuspendLayout();
+            GBP1dragontype.SuspendLayout();
+            GBP2details.SuspendLayout();
+            GBP2dragontype.SuspendLayout();
             SuspendLayout();
             // 
-            // groupBox1
+            // GBP1details
             // 
-            groupBox1.Controls.Add(TXTPlayer1DragonName);
-            groupBox1.Controls.Add(TXTPlayer1Name);
-            groupBox1.Controls.Add(label2);
-            groupBox1.Controls.Add(label1);
-            groupBox1.Controls.Add(groupBox3);
-            groupBox1.Location = new Point(44, 62);
-            groupBox1.Name = "groupBox1";
-            groupBox1.Size = new Size(301, 438);
-            groupBox1.TabIndex = 0;
-            groupBox1.TabStop = false;
-            groupBox1.Text = "groupBox1";
+            GBP1details.Controls.Add(TXTPlayer1DragonName);
+            GBP1details.Controls.Add(TXTPlayer1Name);
+            GBP1details.Controls.Add(label2);
+            GBP1details.Controls.Add(label1);
+            GBP1details.Controls.Add(GBP1dragontype);
+            GBP1details.Location = new Point(44, 62);
+            GBP1details.Name = "GBP1details";
+            GBP1details.Size = new Size(301, 438);
+            GBP1details.TabIndex = 0;
+            GBP1details.TabStop = false;
+            GBP1details.Text = "Player 1 details";
             // 
-            // groupBox2
+            // TXTPlayer1DragonName
             // 
-            groupBox2.Controls.Add(TXTPlayer2DragonName);
-            groupBox2.Controls.Add(groupBox4);
-            groupBox2.Controls.Add(TXTPlayer2Name);
-            groupBox2.Controls.Add(label4);
-            groupBox2.Controls.Add(label3);
-            groupBox2.Location = new Point(368, 62);
-            groupBox2.Name = "groupBox2";
-            groupBox2.Size = new Size(301, 438);
-            groupBox2.TabIndex = 1;
-            groupBox2.TabStop = false;
-            groupBox2.Text = "groupBox2";
+            TXTPlayer1DragonName.Location = new Point(111, 80);
+            TXTPlayer1DragonName.Name = "TXTPlayer1DragonName";
+            TXTPlayer1DragonName.Size = new Size(139, 23);
+            TXTPlayer1DragonName.TabIndex = 4;
             // 
-            // groupBox3
+            // TXTPlayer1Name
             // 
-            groupBox3.Controls.Add(checkBoxP1Water);
-            groupBox3.Controls.Add(checkBoxP1Earth);
-            groupBox3.Controls.Add(checkBoxP1Wind);
-            groupBox3.Controls.Add(checkboxP1fire);
-            groupBox3.Controls.Add(BTNP1Details);
-            groupBox3.Location = new Point(6, 114);
-            groupBox3.Name = "groupBox3";
-            groupBox3.Size = new Size(280, 318);
-            groupBox3.TabIndex = 0;
-            groupBox3.TabStop = false;
-            groupBox3.Text = "groupBox3";
+            TXTPlayer1Name.Location = new Point(111, 28);
+            TXTPlayer1Name.Name = "TXTPlayer1Name";
+            TXTPlayer1Name.Size = new Size(139, 23);
+            TXTPlayer1Name.TabIndex = 3;
             // 
-            // groupBox4
+            // label2
             // 
-            groupBox4.Controls.Add(checkBoxP2Water);
-            groupBox4.Controls.Add(BTNP2Details);
-            groupBox4.Controls.Add(checkBoxP2Earth);
-            groupBox4.Controls.Add(checkBoxP2Wind);
-            groupBox4.Controls.Add(checkBoxP2Fire);
-            groupBox4.Location = new Point(6, 114);
-            groupBox4.Name = "groupBox4";
-            groupBox4.Size = new Size(280, 318);
-            groupBox4.TabIndex = 1;
-            groupBox4.TabStop = false;
-            groupBox4.Text = "groupBox4";
+            label2.AutoSize = true;
+            label2.Location = new Point(16, 83);
+            label2.Name = "label2";
+            label2.Size = new Size(84, 15);
+            label2.TabIndex = 2;
+            label2.Text = "Dragon Name:";
             // 
-            // BTNP1Details
+            // label1
             // 
-            BTNP1Details.Location = new Point(44, 279);
-            BTNP1Details.Name = "BTNP1Details";
-            BTNP1Details.Size = new Size(163, 23);
-            BTNP1Details.TabIndex = 0;
-            BTNP1Details.Text = "Save Player 1 details";
-            BTNP1Details.UseVisualStyleBackColor = true;
+            label1.AutoSize = true;
+            label1.Location = new Point(16, 31);
+            label1.Name = "label1";
+            label1.Size = new Size(77, 15);
+            label1.TabIndex = 1;
+            label1.Text = "Player Name:";
             // 
-            // BTNP2Details
+            // GBP1dragontype
             // 
-            BTNP2Details.Location = new Point(59, 279);
-            BTNP2Details.Name = "BTNP2Details";
-            BTNP2Details.Size = new Size(155, 23);
-            BTNP2Details.TabIndex = 1;
-            BTNP2Details.Text = "Save Player 2 details";
-            BTNP2Details.UseVisualStyleBackColor = true;
+            GBP1dragontype.Controls.Add(checkBoxP1Water);
+            GBP1dragontype.Controls.Add(checkBoxP1Earth);
+            GBP1dragontype.Controls.Add(checkBoxP1Wind);
+            GBP1dragontype.Controls.Add(checkboxP1fire);
+            GBP1dragontype.Controls.Add(BTNP1Details);
+            GBP1dragontype.Location = new Point(6, 114);
+            GBP1dragontype.Name = "GBP1dragontype";
+            GBP1dragontype.Size = new Size(280, 318);
+            GBP1dragontype.TabIndex = 0;
+            GBP1dragontype.TabStop = false;
+            GBP1dragontype.Text = "Player 1 dragon type";
+            // 
+            // checkBoxP1Water
+            // 
+            checkBoxP1Water.AutoSize = true;
+            checkBoxP1Water.Location = new Point(20, 208);
+            checkBoxP1Water.Name = "checkBoxP1Water";
+            checkBoxP1Water.Size = new Size(98, 19);
+            checkBoxP1Water.TabIndex = 4;
+            checkBoxP1Water.Text = "Water dragon";
+            checkBoxP1Water.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxP1Earth
+            // 
+            checkBoxP1Earth.AutoSize = true;
+            checkBoxP1Earth.Location = new Point(20, 159);
+            checkBoxP1Earth.Name = "checkBoxP1Earth";
+            checkBoxP1Earth.Size = new Size(94, 19);
+            checkBoxP1Earth.TabIndex = 3;
+            checkBoxP1Earth.Text = "Earth dragon";
+            checkBoxP1Earth.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxP1Wind
+            // 
+            checkBoxP1Wind.AutoSize = true;
+            checkBoxP1Wind.Location = new Point(20, 110);
+            checkBoxP1Wind.Name = "checkBoxP1Wind";
+            checkBoxP1Wind.Size = new Size(95, 19);
+            checkBoxP1Wind.TabIndex = 2;
+            checkBoxP1Wind.Text = "Wind dragon";
+            checkBoxP1Wind.UseVisualStyleBackColor = true;
             // 
             // checkboxP1fire
             // 
@@ -143,67 +159,28 @@
             checkboxP1fire.UseVisualStyleBackColor = true;
             checkboxP1fire.CheckedChanged += checkBox1_CheckedChanged;
             // 
-            // checkBoxP1Wind
+            // BTNP1Details
             // 
-            checkBoxP1Wind.AutoSize = true;
-            checkBoxP1Wind.Location = new Point(20, 110);
-            checkBoxP1Wind.Name = "checkBoxP1Wind";
-            checkBoxP1Wind.Size = new Size(95, 19);
-            checkBoxP1Wind.TabIndex = 2;
-            checkBoxP1Wind.Text = "Wind dragon";
-            checkBoxP1Wind.UseVisualStyleBackColor = true;
+            BTNP1Details.Location = new Point(44, 279);
+            BTNP1Details.Name = "BTNP1Details";
+            BTNP1Details.Size = new Size(163, 23);
+            BTNP1Details.TabIndex = 0;
+            BTNP1Details.Text = "Save Player 1 details";
+            BTNP1Details.UseVisualStyleBackColor = true;
             // 
-            // checkBoxP1Earth
+            // GBP2details
             // 
-            checkBoxP1Earth.AutoSize = true;
-            checkBoxP1Earth.Location = new Point(20, 159);
-            checkBoxP1Earth.Name = "checkBoxP1Earth";
-            checkBoxP1Earth.Size = new Size(94, 19);
-            checkBoxP1Earth.TabIndex = 3;
-            checkBoxP1Earth.Text = "Earth dragon";
-            checkBoxP1Earth.UseVisualStyleBackColor = true;
-            // 
-            // checkBoxP1Water
-            // 
-            checkBoxP1Water.AutoSize = true;
-            checkBoxP1Water.Location = new Point(20, 208);
-            checkBoxP1Water.Name = "checkBoxP1Water";
-            checkBoxP1Water.Size = new Size(98, 19);
-            checkBoxP1Water.TabIndex = 4;
-            checkBoxP1Water.Text = "Water dragon";
-            checkBoxP1Water.UseVisualStyleBackColor = true;
-            // 
-            // label1
-            // 
-            label1.AutoSize = true;
-            label1.Location = new Point(16, 31);
-            label1.Name = "label1";
-            label1.Size = new Size(77, 15);
-            label1.TabIndex = 1;
-            label1.Text = "Player Name:";
-            // 
-            // label2
-            // 
-            label2.AutoSize = true;
-            label2.Location = new Point(16, 83);
-            label2.Name = "label2";
-            label2.Size = new Size(84, 15);
-            label2.TabIndex = 2;
-            label2.Text = "Dragon Name:";
-            // 
-            // TXTPlayer1Name
-            // 
-            TXTPlayer1Name.Location = new Point(111, 28);
-            TXTPlayer1Name.Name = "TXTPlayer1Name";
-            TXTPlayer1Name.Size = new Size(139, 23);
-            TXTPlayer1Name.TabIndex = 3;
-            // 
-            // TXTPlayer1DragonName
-            // 
-            TXTPlayer1DragonName.Location = new Point(111, 80);
-            TXTPlayer1DragonName.Name = "TXTPlayer1DragonName";
-            TXTPlayer1DragonName.Size = new Size(139, 23);
-            TXTPlayer1DragonName.TabIndex = 4;
+            GBP2details.Controls.Add(TXTPlayer2DragonName);
+            GBP2details.Controls.Add(GBP2dragontype);
+            GBP2details.Controls.Add(TXTPlayer2Name);
+            GBP2details.Controls.Add(label4);
+            GBP2details.Controls.Add(label3);
+            GBP2details.Location = new Point(368, 62);
+            GBP2details.Name = "GBP2details";
+            GBP2details.Size = new Size(301, 438);
+            GBP2details.TabIndex = 1;
+            GBP2details.TabStop = false;
+            GBP2details.Text = "Player 2 details";
             // 
             // TXTPlayer2DragonName
             // 
@@ -212,30 +189,19 @@
             TXTPlayer2DragonName.Size = new Size(139, 23);
             TXTPlayer2DragonName.TabIndex = 8;
             // 
-            // TXTPlayer2Name
+            // GBP2dragontype
             // 
-            TXTPlayer2Name.Location = new Point(118, 28);
-            TXTPlayer2Name.Name = "TXTPlayer2Name";
-            TXTPlayer2Name.Size = new Size(139, 23);
-            TXTPlayer2Name.TabIndex = 7;
-            // 
-            // label3
-            // 
-            label3.AutoSize = true;
-            label3.Location = new Point(23, 83);
-            label3.Name = "label3";
-            label3.Size = new Size(84, 15);
-            label3.TabIndex = 6;
-            label3.Text = "Dragon Name:";
-            // 
-            // label4
-            // 
-            label4.AutoSize = true;
-            label4.Location = new Point(23, 31);
-            label4.Name = "label4";
-            label4.Size = new Size(77, 15);
-            label4.TabIndex = 5;
-            label4.Text = "Player Name:";
+            GBP2dragontype.Controls.Add(checkBoxP2Water);
+            GBP2dragontype.Controls.Add(BTNP2Details);
+            GBP2dragontype.Controls.Add(checkBoxP2Earth);
+            GBP2dragontype.Controls.Add(checkBoxP2Wind);
+            GBP2dragontype.Controls.Add(checkBoxP2Fire);
+            GBP2dragontype.Location = new Point(6, 114);
+            GBP2dragontype.Name = "GBP2dragontype";
+            GBP2dragontype.Size = new Size(280, 318);
+            GBP2dragontype.TabIndex = 1;
+            GBP2dragontype.TabStop = false;
+            GBP2dragontype.Text = "Player 2 dragon type";
             // 
             // checkBoxP2Water
             // 
@@ -246,6 +212,15 @@
             checkBoxP2Water.TabIndex = 8;
             checkBoxP2Water.Text = "Water dragon";
             checkBoxP2Water.UseVisualStyleBackColor = true;
+            // 
+            // BTNP2Details
+            // 
+            BTNP2Details.Location = new Point(59, 279);
+            BTNP2Details.Name = "BTNP2Details";
+            BTNP2Details.Size = new Size(155, 23);
+            BTNP2Details.TabIndex = 1;
+            BTNP2Details.Text = "Save Player 2 details";
+            BTNP2Details.UseVisualStyleBackColor = true;
             // 
             // checkBoxP2Earth
             // 
@@ -278,6 +253,31 @@
             checkBoxP2Fire.Text = "Fire dragon";
             checkBoxP2Fire.UseVisualStyleBackColor = true;
             // 
+            // TXTPlayer2Name
+            // 
+            TXTPlayer2Name.Location = new Point(118, 28);
+            TXTPlayer2Name.Name = "TXTPlayer2Name";
+            TXTPlayer2Name.Size = new Size(139, 23);
+            TXTPlayer2Name.TabIndex = 7;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(23, 31);
+            label4.Name = "label4";
+            label4.Size = new Size(77, 15);
+            label4.TabIndex = 5;
+            label4.Text = "Player Name:";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(23, 83);
+            label3.Name = "label3";
+            label3.Size = new Size(84, 15);
+            label3.TabIndex = 6;
+            label3.Text = "Dragon Name:";
+            // 
             // BTNStartGame
             // 
             BTNStartGame.Location = new Point(230, 545);
@@ -303,37 +303,37 @@
             ClientSize = new Size(943, 625);
             Controls.Add(BTNBack);
             Controls.Add(BTNStartGame);
-            Controls.Add(groupBox2);
-            Controls.Add(groupBox1);
+            Controls.Add(GBP2details);
+            Controls.Add(GBP1details);
             Name = "PlayerInfoScreen";
             Text = "PlayerInfoScreen";
-            groupBox1.ResumeLayout(false);
-            groupBox1.PerformLayout();
-            groupBox2.ResumeLayout(false);
-            groupBox2.PerformLayout();
-            groupBox3.ResumeLayout(false);
-            groupBox3.PerformLayout();
-            groupBox4.ResumeLayout(false);
-            groupBox4.PerformLayout();
+            GBP1details.ResumeLayout(false);
+            GBP1details.PerformLayout();
+            GBP1dragontype.ResumeLayout(false);
+            GBP1dragontype.PerformLayout();
+            GBP2details.ResumeLayout(false);
+            GBP2details.PerformLayout();
+            GBP2dragontype.ResumeLayout(false);
+            GBP2dragontype.PerformLayout();
             ResumeLayout(false);
         }
 
         #endregion
 
-        private GroupBox groupBox1;
+        private GroupBox GBP1details;
         private TextBox TXTPlayer1DragonName;
         private TextBox TXTPlayer1Name;
         private Label label2;
         private Label label1;
-        private GroupBox groupBox3;
+        private GroupBox GBP1dragontype;
         private CheckBox checkBoxP1Water;
         private CheckBox checkBoxP1Earth;
         private CheckBox checkBoxP1Wind;
         private CheckBox checkboxP1fire;
         private Button BTNP1Details;
-        private GroupBox groupBox2;
+        private GroupBox GBP2details;
         private TextBox TXTPlayer2DragonName;
-        private GroupBox groupBox4;
+        private GroupBox GBP2dragontype;
         private CheckBox checkBoxP2Water;
         private Button BTNP2Details;
         private CheckBox checkBoxP2Earth;
