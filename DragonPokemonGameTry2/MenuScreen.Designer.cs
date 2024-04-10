a9c2a44e03a6aaa2917a6a8b2e0950b9c299948c
RaeezLachporia <88308343+RaeezLachporia@users.noreply.github.com>
2024-04-10 14:04:11 +0200

    TRYING AGAIN

--- DragonPokemonGameTry2/MenuScreen.Designer.cs
+++ DragonPokemonGameTry2/MenuScreen.Designer.cs
@@ -28,12 +28,35 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "MenuScreen";
+            textBox1 = new TextBox();
+            SuspendLayout();
+            // 
+            // textBox1
+            // 
+            textBox1.Font = new Font("Segoe UI", 20F, FontStyle.Regular, GraphicsUnit.Point);
+            textBox1.Location = new Point(301, 46);
+            textBox1.Multiline = true;
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(279, 52);
+            textBox1.TabIndex = 0;
+            textBox1.Text = "DRAGONMON";
+            textBox1.TextAlign = HorizontalAlignment.Center;
+            textBox1.TextChanged += textBox1_TextChanged;
+            // 
+            // MenuScreen
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(934, 632);
+            Controls.Add(textBox1);
+            Name = "MenuScreen";
+            Text = "MenuScreen";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private TextBox textBox1;
     }
 }
